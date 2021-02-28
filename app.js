2d1f02057d10e1f51401cf04a6e93ae8492b91e6
Elad <peregrubenselad@gmail.com>
2021-02-28 15:42:14 +0100

    final-project-stock

--- app.js
+++ app.js
@@ -1,23 +1,93 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const exphbs = require("express-handlebars");
+const hb = require("express-handlebars");
+const request = require("request");
+const parser = require("body-parser");
+//
+//
+//middleware - parser
+app.use(parser.urlencoded({ extended: false }));
+//
+//
+//API *public* KEY = pk_875d6d7ec4424488b9db96d0437c364f - export to server.js
+//https://iexcloud.io/console/tokens
+//API request
+function getDataApi(callbackAPI, usrSearchResult) {
+    // request(
+    //     "https://cloud.iexapis.com/stable/stock/" +
+    //         usrSearchResult +
+    //         "/quote?token=pk_875d6d7ec4424488b9db96d0437c364f",
+    request(
+        "https://cloud.iexapis.com/stable/stock/" +
+            usrSearchResult +
+            "/quote?token=pk_875d6d7ec4424488b9db96d0437c364f",
+        { json: true },
+        (err, res, body) => {
+            if (err) {
+                return console.log(err);
+                // console.log(app.js - Line 29 body);
+            }
+            if (res.statusCode === 200) {
+                console.log("app.js - Line 32 - body is", body);
+                // return body;
+                callbackAPI(body);
+            }
+        }
+    );
+}
 
-const PORT = process.env.PORT || 8080;
+//
+//
 
 //Handlebars Middleware
-app.engine("handlebars", exphbs());
+app.engine("handlebars", hb());
 app.set("view engine", "handlebars");
 
-//set Handlebars-route
-
+//Routes
+//HOME GET page Handlebars-route
 app.get("/", function (req, res) {
-    res.render("home", {
-        stuff: "this is stuff...",
-    });
+    getDataApi(function (callback) {
+        res.render("home", {
+            apiInfo: callback,
+        });
+    }, "fb");
+    // console.log(apiData);
+});
+
+//HOME - POST page Handlebars-route
+app.post("/", function (req, res) {
+    getDataApi(function (callback) {
+        const searchQ = req.body.ticker;
+        res.render("home", {
+            apiInfo: callback,
+            searchQ: searchQ,
+        });
+        console.log(
+            "app.js - Line 66 - searchQ (req.body.ticker) var is : ",
+            searchQ
+        );
+    }, req.body.ticker);
+    // }, req.body.ticker); //    getDataApi(function, req.body.stock));
+    // console.log(apiData);
+});
+
+//ABOUT GET  page route
+app.get("/about.html", function (req, res) {
+    res.render("about");
 });
 
 //static folder
 app.use(express.static(path.join(__dirname, "./public")));
+//
+//
 
-app.listen(PORT, () => console.log("server is listening on " + PORT));
+//
+//
+//
+//
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () =>
+    console.log("final project - server is listening on " + PORT)
+);
